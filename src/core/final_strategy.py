b742229845c2c2be22d07f6a8921605cfce30712
Brandon Johnson <brandon.johnson.official@gmail.com>
2018-01-18 05:16:24 -0500

    ~

--- src/core/final_strategy.py
+++ src/core/final_strategy.py
@@ -1,11 +1,18 @@
-from statistics import *
+from core.stats.standard_deviation import simple_standard_deviation
+from core.stats.mean import simple_mean
 
 
 # =============================================================================
 # assist functions
 # =============================================================================
 def exists_on_my_exchange(name):
-    return
+    # get list of exchange from extraced scrape of coin's available exchanges
+    tradable = []
+    my_exchanges = open(exchanges).read()
+    for exchange in extract_exchanges(name):
+        if exchange in my_exchanges:
+            tradable.append(exchange)
+
 
 # =============================================================================
 # ultimate strategy
@@ -15,9 +22,27 @@ def exists_on_my_exchange(name):
 # =============================================================================
 # actions
 # =============================================================================
-def sell(symbol, current_price):  # statistically calculated buy price
-    return mean(symbol, current_price) - stddev(symbol)
+def sell(symbol, current_price):
+    return simple_mean(symbol, current_price) - simple_standard_deviation(symbol)
 
 
-def buy(symbol, current_price):  # statistically calculated buy price
-    return mean(symbol, current_price) + stddev(symbol)
+def buy(symbol, current_price):
+    return simple_mean(symbol, current_price) + simple_standard_deviation(symbol)
+
+
+def update():
+    f = open(watchdata, 'a')
+    date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    for crypto in fetch_watchlist():
+        price = float(Market().coin(crypto)[0]['price_usd'])  # * unicode bug
+        symbol = str(Market().coin(crypto)[0]['symbol'])  # * unicode bug
+        csv.writer(f).writerow([date, price, symbol])
+        if price < sell(symbol, price):
+            print_fail(symbol + ' $' + str(price))
+        elif price > buy(symbol, price):
+            print_pass(symbol + ' $' + str(price))
+        else:
+            print symbol + ' $' + str(price)
+    f.write('\n')
+    f.close()
+    # maybe this can be optimized so you don't have to open the file every update
