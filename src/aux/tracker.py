8a6eda9ebb2a766c135f34f00d32f29c64498ee6
Brandon Johnson <brandon.johnson.official@gmail.com>
2018-02-18 17:49:09 -0500

    ~

--- src/aux/tracker.py
+++ src/aux/tracker.py
@@ -1,6 +1,8 @@
-from bs4 import BeautifulSoup
 from generic import print_warn
 from generic import tracker
+
+from bs4 import BeautifulSoup
+
 import requests
 import json
 
@@ -11,42 +13,36 @@ soup = BeautifulSoup(requests.get('https://coinmarketcap.com').text, 'lxml')
 symbols = soup.select('.circulating-supply a span.hidden-xs')
 change = soup.find_all(class_='percent-change')
 
-data = []
 cycle = 0
+load = []
 
 
-def current_percents():
-    percents = []
-    for i in range(100):
-        percents.append(float(change[i].text[:-1]))
-    return sorted(percents, reverse=True) 
+def mov(i, sym):
+    # here what we want to get is the past keys using the old vals[0], 
+    # so by finding the symbol, we know what the past % was -- we get 
+    # the current % by passing through the data
+    print load[i].items()[0][1][0]
 
-
-def movement(index):
-    keys = []
-    syms = []
-    movs = []
-    with open(tracker, 'r') as f:
-        load = json.load(f)
-        for i in range(99):
-            key = data[i].keys()
-            syms.append(data[i][key[0]][0])
-            movs.append(data[i][key[0]][1])
-            keys.append(float(key[0]))
-
-
-def portfolio_matches():  # maybe belongs in a different file
-    return
+    shift = int(load[i][load[i].keys()[0]][1])
+    #print str(shift) + " " + load[i][load[i].keys()[0]][0]
+    if float(load[i].keys()[0]) < percent:
+        shift += 1
+    if float(load[i].keys()[0]) > percent:
+        shift -= 1
+    return shift
 
 
 def track(empty):
+    data = []
+    sym = ''
     if empty:
         for i in range(100):
-            data.append({percents[i]:[symbols[i].text, 0]})
+            sym = symbols[i].text
+            data.append({float(change[i].text[:-1]):sym, sym:0})
     else:
         for i in range(100):
-            data.append({percents[i]:[symbols[i].text,
-                                      movement(percents[i])]})
+            sym = symbols[i].text
+            data.append({float(change[i].text[:-1]):sym, sym:mov(i, sym)]})
     fout = open(tracker, 'w')
     #fout.write(str(cycle+1))
     fout.write(json.dumps(sorted(data, reverse=True)))
@@ -56,9 +52,18 @@ def track(empty):
 def init():
     if os.path.isfile(tracker) and os.stat(tracker).st_size != 0:
         #cycle = int([x for x in open(tracker, 'r')][0][0])
+        with open(tracker, 'r') as f:
+            global load
+            load = json.load(f)
+            f.close()
         track(False)
     else:
         print_warn('no tracker data; initializing first cycle...')
         track(True)
 
+# what I essentially want to see are a list of symbols highlighted 
+# green, red, or default indicating that the trailing is working, 
+# perhaps, what I'll do is just place a + or - sign next to my 
+# portfolio coins when their ROC is significantly moving in 
+# some direction
 init()
